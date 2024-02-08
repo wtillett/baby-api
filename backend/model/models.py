b501d85c7b8d1f64bf923c4615da1a4e89684f5e
Will Tillett <wtillett@gmail.com>
2024-02-08 10:02:40 -0800

    UI Updates

--- backend/model/models.py
+++ backend/model/models.py
@@ -18,3 +18,14 @@ class Bottle(Base):
 
     def __repr__(self):
         return f"Bottle (id={self.id}, date={self.date}, time={self.time}, amount={self.amount})"
+
+
+class Sleep(Base):
+
+    __tablename__ = "sleeps"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    date = Column(String)
+    start = Column(String)
+    end = Column(String)
+    
